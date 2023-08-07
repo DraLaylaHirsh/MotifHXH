c1c0e66ebc768b525d1c98eb06e89cfac9600ff5
DraLaylaHirsh <111517699+DraLaylaHirsh@users.noreply.github.com>
2023-08-07 17:00:51 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,37 +6,6 @@ project = u'Motif HxH'
 copyright = u'2023, Layla Hirsh'
 author = u'Layla Hirsh'
 
-
-release = '1'
-version = '1.0'
-
-# -- General configuration
-html_theme = 'sphinx_rtd_theme'
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-
-# -- Options for HTML output
-
-#html_theme = 'sphinx_rtd_theme'
-#html_logo = '4gg4.png'
-
-latex_logo =  'REFRACT.png'
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
-
 html_theme_options = {
     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
@@ -53,4 +22,3 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
-
