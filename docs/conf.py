0db3a8c4b007e3631f8bd5e5252cd7aa424cc7a2
DraLaylaHirsh <111517699+DraLaylaHirsh@users.noreply.github.com>
2023-08-07 17:03:35 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,19 +6,9 @@ project = u'Motif HxH'
 copyright = u'2023, Layla Hirsh'
 author = u'Layla Hirsh'
 
-html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+
+extensions = [
++    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints'
+]
